f1a0b92844bdd30dcca4590c9b1e7cd1af9d3fa7
Mathieu Benoit <mabenoit@microsoft.com>
2017-07-10 12:40:06 -0400

    Init folders: infra and test.

---
+++ src/DotNetFunction/GenerateDeployToAzureUriFunction.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Net;
+using System.Net.Http;
+
+namespace DotNetFunction
+{
+    public static class GenerateDeployToAzureUriFunction
+    {
+        [FunctionName("HttpTriggerWithParametersCSharp")]
+
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "HttpTriggerCSharp/name/{name}")]HttpRequestMessage req, string name, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            // Fetching the name from the path parameter in the request URL
+            return req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        }
+    }
+}
---
+++ test/DotNetFunction.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetFunction.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetFunction.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1674604-9fdc-4033-bf0e-ade1ac5fafba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
