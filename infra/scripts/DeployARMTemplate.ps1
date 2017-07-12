8fa67bff56e93a3ef80b88f87d61194e890b86eb
Mathieu Benoit <mabenoit@microsoft.com>
2017-07-12 16:07:02 -0400

    Add powershell script: lock and arm deployment.

---
+++ infra/scripts/DeployARMTemplate.ps1
@@ -0,0 +1,15 @@
+﻿#Powershell script to use locally as example. Let's adapt it for your own needs.
+
+Param(
+    [string] $ResourceGroupLocation = 'East US',
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $TemplateFile = '..\templates\AppServicePlan.json'
+)
+
+#Login-AzureRmAccount
+#Select-AzureSubscription "MySubscription"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateFile $TemplateFile `
+									   -Force -Verbose
