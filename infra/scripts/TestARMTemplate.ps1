49c95703b98a9002cccb3928401b7457c941f1b7
Mathieu Benoit <mabenoit@microsoft.com>
2017-07-13 09:38:08 -0400

    Implement Test ARM Templates script + fix parameters.

---
+++ infra/scripts/TestARMTemplate.ps1
@@ -0,0 +1,16 @@
+﻿#Powershell script to use locally as example. Let's adapt it for your own needs.
+
+Param(
+    [string] $ResourceGroupLocation = 'East US',
+    [string] $TemplateFile = '..\templates\deploy.json'
+)
+
+$temporaryResourceGroupName = "tmp-rg-for-templates-validation"
+
+#Login-AzureRmAccount;
+#Select-AzureRmSubscription -SubscriptionId $SubscriptionId;
+New-AzureRmResourceGroup -Name $temporaryResourceGroupName -Location $ResourceGroupLocation;
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $temporaryResourceGroupName `
+                                       -TemplateFile $TemplateFile `
+                                       -Verbose;
+Remove-AzureRmResourceGroup -Name $temporaryResourceGroupName;
