a076d059b41491c397d2a6d9d2a97afd54c8baa2
Mathieu Benoit <mabenoit@microsoft.com>
2017-08-01 11:02:30 -0400

    Create a new UnitTests project with xUnit.

---
+++ test/DotNetFunction.UnitTests2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace DotNetFunction.UnitTests2
+{
+    public class SampleHelloDotNetFunctionTests
+    {
+        [Fact]
+        public void EmptyNameShouldReturnHelloWorld()
+        {
+            //Arrange
+            var name = string.Empty;
+
+            //Act
+            var result = SampleHelloDotNetFunction.InternalSampleHelloDotNetFunction(name);
+
+            //Assert
+            Assert.Equal(result, "Hello, World!");
+        }
+
+        [Fact]
+        public void NotEmptyNameShouldReturnAppropriateHello()
+        {
+            //Arrange
+            var name = "John";
+
+            //Act
+            var result = SampleHelloDotNetFunction.InternalSampleHelloDotNetFunction(name);
+
+            //Assert
+            Assert.Equal(result, $"Hello, {name}!");
+        }
+    }
+}
