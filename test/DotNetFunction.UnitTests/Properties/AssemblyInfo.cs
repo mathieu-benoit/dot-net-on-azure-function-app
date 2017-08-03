cbeda135de52b9f3b46522aaa3463b5230f7e330
Mathieu Benoit <mabenoit@microsoft.com>
2017-08-03 14:19:28 -0400

    No more *Tests2 occurences.

--- test/DotNetFunction.UnitTests/Properties/AssemblyInfo.cs
+++ test/DotNetFunction.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNetFunction.UnitTests2")]
+[assembly: AssemblyTitle("DotNetFunction.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetFunction.UnitTests2")]
+[assembly: AssemblyProduct("DotNetFunction.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
