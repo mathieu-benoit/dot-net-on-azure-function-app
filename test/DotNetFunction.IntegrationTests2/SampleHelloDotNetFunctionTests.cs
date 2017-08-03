0a873d7816cfb83fa40268499b5c09900eb8269b
Mathieu Benoit <mabenoit@microsoft.com>
2017-08-03 14:08:10 -0400

    Integration tests with the new Function's body.

--- test/DotNetFunction.IntegrationTests2/SampleHelloDotNetFunctionTests.cs
+++ test/DotNetFunction.IntegrationTests2/SampleHelloDotNetFunctionTests.cs
@@ -7,7 +7,7 @@ namespace DotNetFunction.IntegrationTests
 {
     public class Class1
     {
-        private string BaseUrl = "https://localhost:7071/api/SampleHelloDotNetFunction";
+        private string BaseUrl = "https://mabenoittest.azurewebsites.net/api/SampleHelloDotNetFunction";//"https://localhost:7071/api/SampleHelloDotNetFunction";
 
         public Class1()
         {
@@ -17,7 +17,7 @@ namespace DotNetFunction.IntegrationTests
         }
 
         [Fact]
-        public async Task EmptyNameShouldSendNotFound()
+        public async Task Get_EmptyName_ShouldSendBadRequestAndHelpMessage()
         {
             //Arrange
             var httpClient = new HttpClient();
@@ -25,9 +25,43 @@ namespace DotNetFunction.IntegrationTests
 
             //Act
             var response = await httpClient.GetAsync(urlTested);
+            var text = await response.Content.ReadAsStringAsync();
 
             //Assert
-            Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.NotFound);
+            Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.BadRequest);
+            Assert.Equal(text, "Please pass a name on the query string or in the request body.");
+        }
+
+        [Fact]
+        public async Task Post_ShouldSendNotAllowed()
+        {
+            //Arrange
+            var httpClient = new HttpClient();
+            var urlTested = BaseUrl;
+            var name = "test";
+            var httpContent = new StringContent($"name={name}");
+
+            //Act
+            var response = await httpClient.PostAsync(urlTested, httpContent);
+
+            //Assert
+            Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.MethodNotAllowed);
+        }
+
+        [Fact]
+        public async Task Put_ShouldSendNotAllowed()
+        {
+            //Arrange
+            var httpClient = new HttpClient();
+            var urlTested = BaseUrl;
+            var name = "test";
+            var httpContent = new StringContent($"name={name}");
+
+            //Act
+            var response = await httpClient.PutAsync(urlTested, httpContent);
+
+            //Assert
+            Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.MethodNotAllowed);
         }
 
         [Fact]
@@ -35,8 +69,8 @@ namespace DotNetFunction.IntegrationTests
         {
             //Arrange
             var httpClient = new HttpClient();
-            var nameTested = "john";
-            var urlTested = $"{BaseUrl}/{nameTested}";
+            var name = "john";
+            var urlTested = $"{BaseUrl}/{name}";
 
             //Act
             var response = await httpClient.GetAsync(urlTested);
@@ -44,7 +78,7 @@ namespace DotNetFunction.IntegrationTests
 
             //Assert
             Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.OK);
-            Assert.Equal(text, $"\"Hello, {nameTested}!\"");
+            Assert.Equal(text, $"\"Hello, {name}!\"");
         }
 
     }
